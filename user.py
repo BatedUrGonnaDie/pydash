69082801f34034e23eb0db10ccd083a49884f145
BatedUrGonnaDie <batedurgonnadie@yahoo.com>
2015-04-05 04:38:19 -0700

    Redesign user class
    display name optional param now, moving chat color to user class allows
    to set a permanent color to chatters who do not have a color set

--- user.py
+++ user.py
@@ -1,7 +1,12 @@
 #! /usr/bin/env python2.7
 
-class User:
+import random
 
-    def __init__(self, username, display_name):
+DEFAULT_COLORS = ["Blue", "BlueViolet", "CadetBlue", "Chocolate", "Coral", "DodgerBlue", "Firebrick", "GoldenRod", "Green", "HotPink", "OrangeRed", "Red", "SeaGreen", "SpringGreen", "YellowGreen"]
+
+class User(object):
+
+    def __init__(self, username, chat_color, display_name=None):
         self.name = username
         self.display_name = display_name
+        self.chat_color = chat_color if chat_color is not "" else random.choice(DEFAULT_COLORS)
